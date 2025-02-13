e1a250c87010171a7512cc47fb573b9d61b0077f
alcides94 <alcidesquinterosugarte94@gmail.com>
2025-02-13 07:30:57 +0100

    react

--- React-Proyectos/juegoreact/src/App.js
+++ React-Proyectos/juegoreact/src/App.js
@@ -1,68 +1,60 @@
-
-import './App.css';
-import BtnReiniciar from './componentes/btnReiniciar.js';
-import Adivina from './componentes/adivina.js';
-import Check from './componentes/check.js';
-import Contenido from './componentes/Textos';
-
+import logo from './logo.svg';
+import BtnReiniciar from './components/BtnReiniciar';
+import Adivina from './components/Adivina';
+import Formulario from './components/Formulario';
+import Index from './components/Textos';
 import { useState } from 'react';
 
-function App() {
+import './App.css';
 
-  function generarNumeroSecreto () {
+function App() {
+  
+  const generarNumSecreto = () => {
     return Math.trunc(Math.random()*20)+1;
   }
 
-
-  const [numSecreto,setNumSecreto]=useState(generarNumeroSecreto);
-  const [mensaje,setMensaje]=useState("Empieza a adivinar ....");
-  const [puntuacion,setPuntuacion]=useState(20);
-  const [puntajeAlto,setPuntajeAlto]=useState(0);
-
-  const reiniciar =()=>{
-    setNumSecreto(generarNumeroSecreto);
-    setMensaje("Empieza a adivinar ....");
-    setPuntuacion=useState(20);
-    setPuntajeAlto=useState(0);
+  const [numSecreto, setNumSecreto] = useState(generarNumSecreto);
+  const [mensaje, setMensaje] = useState("Empieza a adivinar...");
+  const [puntuacion, setPuntuacion] = useState(20);
+  const [puntuacionAlta, setPuntuacionAlta] = useState(0);
+  
+  const reiniciar = () =>{
+    setNumSecreto(generarNumSecreto);
+    setMensaje("Empieza a adivinar...");
+    setPuntuacion(20);
+    document.body.style.backgroundColor = "#222";
   }
 
-  const comparar=(num)=>{
-    if(!num){
-      setMensaje("escribe un numero");
-    }else {
-      if (Number(num)==numSecreto) {
-        /* Ganado */
-        setMensaje("Has Ganado");
-        document.body.style.backgroundColor="green";
-      }else {
-        if (puntuacion>1) {
-          if (Number(num)>numSecreto) {
-            /* Ganado */
-            setMensaje("Tu numero es alto");
-          }else {
-            setMensaje("Tu numero es bajo");
-          }
-          setPuntuacion(puntuacion-1);
-        }else {
-          setMensaje("Has perdido")
-          document.body.style.backgroundColor="red";
+  const comparar = (num) =>{
+    if (!num) setMensaje("Debes introducir un número");
+    else{
+      if(Number(num) == numSecreto) {
+        setMensaje("Has ganado");
+        document.body.style.backgroundColor = "green";
+        if(puntuacion > puntuacionAlta) setPuntuacionAlta(puntuacion);
+      }
+      else {
+        if(puntuacion > 1 ){
+            if(Number(num) > numSecreto) setMensaje("Tu número es alto");
+            else setMensaje("Tu número es bajo")
+            setPuntuacion(puntuacion-1);
+        }else{
+          setMensaje("Has perdido!!");
+          document.body.style.backgroundColor = "red";
         }
       }
     }
-
   }
-
 
   return (
     <div className="container">
       <BtnReiniciar reiniciar={reiniciar}/>
-    <h1>¡Adivina mi número! entre 1 y 20</h1>{numSecreto}
-      <Adivina/>
-    <br/>
-    
-    <Check comparar={comparar}/>
-    <Contenido mensaje={mensaje} puntuacion={puntuacion} puntajeAlto={puntajeAlto}/>
-  </div>
+      <h1>¡Adivina mi número entre 1 y 20!</h1> {numSecreto}
+      <Adivina />
+      <br />
+      <Formulario comparar={comparar}/>
+      <Index mensaje={mensaje} puntuacion={puntuacion} puntuacionAlta={puntuacionAlta}/>
+    </div>
   );
 }
 
